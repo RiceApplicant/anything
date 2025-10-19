9004829b4a6e963d389cbac49faf94169f024630
RiceApplicant <el2123@jagmail.southalabama.edu>
2025-10-18 20:22:08 -0500

    Add files via upload

---
+++ PA-5-Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <chrono>
+
+
+void printPath(vector<int> arr, int option){
+    cout << endl << "Option " << option << " ";
+    for(val : arr){
+        cout << "Task_" << val << "->";
+    }
+    cout << endl;
+}
+
+
+vector<int> bruteForce(vector<int> arr){
+    vector<int> path;
+
+    return path;
+}
+
+
+
+
+
+
+int main(){
+    return 0;
+}
